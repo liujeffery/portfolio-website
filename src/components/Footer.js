d122117f5bf913b986748c20160b3c0bfac4b9d1
unknown <jefferyliu521@gmail.com>
2023-05-31 18:26:53 -0400

    changing title and icon for website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Github from "@mui/icons-material/GitHub"
 import LinkedIn from "@mui/icons-material/LinkedIn"
+import IconButton from "@mui/material/IconButton"
+import {Link} from "react-router-dom" 
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className = "footer">
         <div className = "socialMedia">
-            <Github />
-            <LinkedIn />
+          <Link to="https://github.com/liujeffery">
+            <IconButton>
+              <Github />
+            </IconButton>
+          </Link>
+          <Link to="https://www.linkedin.com/in/jeffery-liu/">
+            <IconButton>
+              <LinkedIn />
+            </IconButton> 
+          </Link>
+          
         </div>
         <p>@2023 Jeffery Liu</p>
     </div>
