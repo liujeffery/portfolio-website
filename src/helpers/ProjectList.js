5e426cd9e3a99bb6c19dd582cde45b59ff48a95f
unknown <jefferyliu521@gmail.com>
2024-03-18 17:55:38 -0400

    adding urbanism and piraten karpen as projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,8 @@
 import benedict from "../assets/benedict.png";
-import money_manager from "../assets/money_manager.png"
-import portfolio_website from "../assets/portfolio_website.png"
+import money_manager from "../assets/money_manager.png";
+import portfolio_website from "../assets/portfolio_website.png";
+import a4_urbanism from "../assets/a4_urbanism.png";
+import a1_piraten from "../assets/a1_piraten.png";
 
 export const ProjectList = [
     {
@@ -18,10 +20,24 @@ export const ProjectList = [
         skills: ["React.js", "Material UI", "Github", "Javascript", "HTML", "CSS"]
     },
     {
+        name: "Urbanism",
+        image: a4_urbanism,
+        link: "https://github.com/2AA4-W23/a4-urbanism-liujeffery",
+        description: "A command line interface that procedurally generates an island, complete with accurate biomes, humidity, elevation, and temperature. The user can define specific properties through the command line, or have them be randomly generated. Cities are also added, with roads connecting them that are calculated using Dijkstra's algorithm. Completed as an assignment for the 2AA4 course.",
+        skills: ["Apache CLI", "Mesh Generation", "Maven", "Log4j", "Java"]
+    },
+    {
+        name: "Piraten Karpen",
+        image: a1_piraten,
+        link: "https://github.com/2AA4-W23/a1-liujeffery",
+        description: "A command line interface that simulates games of Piraten Karpen. The program includes the option to specify strategies for each player and apply various rulesets to the simulation. Completed as an assignment for the 2AA4 course.",
+        skills: ["Apache CLI", "Maven", "Log4j", "Java"]
+    },
+    {
         name: "Money Manager",
         image: money_manager,
         link: "https://github.com/liujeffery/Financial-Tracker/tree/main",
-        description: "A Java application built as an internal assessment for the ISC4U7 course, that aids in financial management. The app includes features to add individual and continuous expenses, and compiles relevant information and statistics in graphic form.",
+        description: "An application built through Java that aids in financial management. The app includes features to add individual and continuous expenses, and compiles relevant information and statistics in graphic form. Made as an internal assessment for the ISC4U7 course. ",
         skills: ["Java Swing", "Java"]
     },
 ]
